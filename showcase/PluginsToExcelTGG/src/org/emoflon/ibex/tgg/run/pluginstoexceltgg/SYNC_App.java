695d5d25207abf669746afe678c45d68b3d8bceb
srijani <srijanighosh87@gmail.com>
2018-04-27 16:00:37 +0200

    Committing changes.

--- showcase/PluginsToExcelTGG/src/org/emoflon/ibex/tgg/run/pluginstoexceltgg/SYNC_App.java
+++ showcase/PluginsToExcelTGG/src/org/emoflon/ibex/tgg/run/pluginstoexceltgg/SYNC_App.java
@@ -3,27 +3,32 @@ package org.emoflon.ibex.tgg.run.pluginstoexceltgg;
 import java.io.IOException;
 
 import org.apache.log4j.BasicConfigurator;
-
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.csp.constraints.factories.UserDefinedRuntimeTGGAttrConstraintFactory;
 import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
 import org.emoflon.ibex.tgg.operational.strategies.sync.SYNC;
 import org.emoflon.ibex.tgg.runtime.engine.DemoclesTGGEngine;
 
 public class SYNC_App extends SYNC {
-
-	public SYNC_App() throws IOException {
+	private boolean fwd;
+	
+	public SYNC_App(boolean fwd) throws IOException {
 		super(createIbexOptions());
+		this.fwd = fwd;
 		registerBlackInterpreter(new DemoclesTGGEngine());
 	}
 
 	public static void main(String[] args) throws IOException {
 		BasicConfigurator.configure();
 
-		SYNC_App sync = new SYNC_App();
+		SYNC_App sync = new SYNC_App(false);
 		
 		logger.info("Starting SYNC");
 		long tic = System.currentTimeMillis();
-		sync.forward();
+		
+		//sync.forward();
+		sync.chooseTransformationDirection(sync);
+		
 		long toc = System.currentTimeMillis();
 		logger.info("Completed SYNC in: " + (toc - tic) + " ms");
 		
@@ -31,6 +36,40 @@ public class SYNC_App extends SYNC {
 		sync.terminate();
 	}
 
+	private void chooseTransformationDirection(SYNC_App sync) {
+
+		try {
+			if (this.fwd) {
+				System.out.println("Performing forward transformation");
+				sync.forward();
+			}
+			else {
+				System.out.println("Performing backward transformation");
+				sync.backward();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Override
+	public void loadModels() throws IOException {
+		if (fwd) {
+			s = loadResource(projectPath + "/instances/src.xmi");
+			t = createResource(projectPath + "/instances/trg.xmi");
+		} else {
+			t = loadResource(projectPath + "/instances/trg.xmi");
+			s = createResource(projectPath + "/instances/src.xmi");
+		}
+
+		c = createResource(projectPath + "/instances/corr.xmi");
+		p = createResource(projectPath + "/instances/protocol.xmi");
+
+		EcoreUtil.resolveAll(rs);
+	}
+
+	
 	protected void registerUserMetamodels() throws IOException {
 		_RegistrationHelper.registerMetamodels(rs, this);
 			
--- showcase/PluginsToExcelTGG/src/org/emoflon/ibex/tgg/run/pluginstoexceltgg/_RegistrationHelper.java
+++ showcase/PluginsToExcelTGG/src/org/emoflon/ibex/tgg/run/pluginstoexceltgg/_RegistrationHelper.java
@@ -7,27 +7,19 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.emoflon.ibex.tgg.operational.strategies.OperationalStrategy;
 
+import Simpleexcel.impl.SimpleexcelPackageImpl;
+import Simpletree.impl.SimpletreePackageImpl;
+
 
 public class _RegistrationHelper {
 
 	/** Load and register source and target metamodels */
 	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy)  throws IOException {
-		throw new NotImplementedException("You need to register your source and target metamodels.");
+		//throw new NotImplementedException("You need to register your source and target metamodels.");
 		
-		// For both source and target metamodels (and any other dependencies you might require)
-		
-		// Option 1 (recommended): If you have generated code for your metamodel <Foo> and use eMoflon projects and defaults,
-		//                         just add the project Foo as a plugin dependency and simply use:
-		// FooPackageImpl.init();
-
-		// Option 2:  If you wish to use the .ecore file directly without generating code
-		// strategy.loadAndRegisterMetamodel("<pathToEcoreFile>");
-		
-		// Option 3 (advanced): If you have an .ecore file with an arbitrary URI "<URIOfPackage>"
-		// String pathToEcoreFile = "<pathToEcoreFile>";
-		// URI key = URI.createURI("<URIOfPackage>");
-		// URI value = URI.createURI(pathToEcoreFile);
-		// strategy.loadAndRegisterMetamodel(pathToEcoreFile);
-		// rs.getURIConverter().getURIMap().put(key, value);
+		rs.getPackageRegistry().put("platform:/plugin/com.kaleidoscope.core.aux.simpleexcel/model/Simpleexcel.ecore",
+				SimpleexcelPackageImpl.init());
+		rs.getPackageRegistry().put(
+				"platform:/plugin/com.kaleidoscope.core.aux.simpletree/model/Simpletree.ecore", SimpletreePackageImpl.init());
 	}
 }
